05d7ac4f4e74643dd828574f5020f67f57369165
Pete Correia <pete@petecorreia.com>
2018-07-29 09:45:40 +0100

    Better column names for R

--- distance.R
+++ distance.R
@@ -5,13 +5,13 @@ library(zoo)
 
 Data <- read.csv("data/multiple-daily-metrics.csv")
 Data$date <- as.Date(Data$start, format = "%d-%b-%Y")
-Data$distance <- as.numeric(Data$distance)
-Data$distance_ma <- rollmean(Data$distance, k = 30, fill = NA)
+Data$distance_km <- as.numeric(Data$distance_km)
+Data$distance_km_ma <- rollmean(Data$distance_km, k = 30, fill = NA)
 
 Data %>% 
   ggplot() +
-    geom_point(aes(date, distance), alpha = 0.25, size = 0.25) +
-    geom_line(aes(date, distance_ma), color = 'red') +
+    geom_point(aes(date, distance_km), alpha = 0.25, size = 0.25) +
+    geom_line(aes(date, distance_km_ma), color = 'red') +
     scale_x_date(breaks = date_breaks("6 months"),
                  labels = date_format("%b %Y")) +
     labs(title = "Distance", x = "", y = "KM", color = "Type") + 
